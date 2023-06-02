d668e3cc95047edf2f8597ba8c45e5b494907c4c
GenjiFujimoto <dadra102@hhu.de>
2023-06-02 18:45:14 +0200

    Small improvements, close with right click

--- popup.c
+++ popup.c
@@ -1,8 +1,21 @@
 #include <gtk/gtk.h>
 
+#define BORDER_WIDTH 2
+#define MARGIN 5
+
 static void destroy(GtkWidget *widget, gpointer data)
 {
     gtk_main_quit();
+}
+
+static gboolean on_button_press(GtkWidget *widget, GdkEventButton *event, gpointer user_data)
+{
+    if (event->button == GDK_BUTTON_SECONDARY) {
+	GtkWidget *window = gtk_widget_get_toplevel(widget);
+        gtk_widget_destroy(window);
+        return TRUE;
+    }
+    return FALSE;
 }
 
 int main(int argc, char *argv[])
@@ -17,25 +30,35 @@ int main(int argc, char *argv[])
       text[len - 1] = '\0';
 
     gtk_init(&argc, &argv);
-    
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "floating");
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_POPUP);
+    gtk_window_set_title(GTK_WINDOW(window), "dictpopup");
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
     gtk_window_set_keep_above(GTK_WINDOW(window), 1);
+    gtk_container_set_border_width(GTK_CONTAINER(window), BORDER_WIDTH);
     g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
     
     GtkTextBuffer *textbuffer = gtk_text_buffer_new(NULL);
     gtk_text_buffer_set_text(textbuffer, text, -1);
     
     GtkWidget *textview = gtk_text_view_new_with_buffer(textbuffer);
+
+    gtk_text_view_set_top_margin(GTK_TEXT_VIEW(textview), MARGIN);
+    gtk_text_view_set_bottom_margin(GTK_TEXT_VIEW(textview), MARGIN);
+    gtk_text_view_set_left_margin(GTK_TEXT_VIEW(textview), MARGIN);
+    gtk_text_view_set_right_margin(GTK_TEXT_VIEW(textview), MARGIN);
+
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
     gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textview), GTK_WRAP_CHAR);
     gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(textview), FALSE);
     gtk_widget_set_size_request(textview, 480, -1);
     gtk_container_add(GTK_CONTAINER(window), textview);
+
+    g_signal_connect(textview, "button-press-event", G_CALLBACK(on_button_press), NULL);
     
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_MOUSE);
     gtk_widget_show_all(window);
-    
+
     gtk_main();
     
     return 0;
