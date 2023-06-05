17551446925fcd11fd29b64a233a40b8531f44f1
GenjiFujimoto <dadra102@hhu.de>
2023-06-05 16:12:15 +0200

    Switch from popup to window for now

--- popup.c
+++ popup.c
@@ -2,20 +2,13 @@
 
 #define BORDER_WIDTH 2
 #define MARGIN 5
+#define MAX_WIDTH 480
+
+GtkCssProvider *provider;
 
 static void destroy(GtkWidget *widget, gpointer data)
 {
     gtk_main_quit();
-}
-
-static gboolean on_button_press(GtkWidget *widget, GdkEventButton *event, gpointer user_data)
-{
-    if (event->button == GDK_BUTTON_SECONDARY) {
-	GtkWidget *window = gtk_widget_get_toplevel(widget);
-        gtk_widget_destroy(window);
-        return TRUE;
-    }
-    return FALSE;
 }
 
 int main(int argc, char *argv[])
@@ -31,11 +24,12 @@ int main(int argc, char *argv[])
 
     gtk_init(&argc, &argv);
 
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_POPUP);
-    gtk_window_set_title(GTK_WINDOW(window), "dictpopup");
+    /* GtkWidget *window = gtk_window_new(GTK_WINDOW_POPUP); */
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "floating");
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
     gtk_window_set_keep_above(GTK_WINDOW(window), 1);
-    gtk_container_set_border_width(GTK_CONTAINER(window), BORDER_WIDTH);
+    /* gtk_container_set_border_width(GTK_CONTAINER(window), BORDER_WIDTH); */
     g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
     
     GtkTextBuffer *textbuffer = gtk_text_buffer_new(NULL);
@@ -51,10 +45,18 @@ int main(int argc, char *argv[])
     gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
     gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textview), GTK_WRAP_CHAR);
     gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(textview), FALSE);
-    gtk_widget_set_size_request(textview, 480, -1);
-    gtk_container_add(GTK_CONTAINER(window), textview);
+    gtk_widget_set_size_request(textview, MAX_WIDTH, -1);
 
-    g_signal_connect(textview, "button-press-event", G_CALLBACK(on_button_press), NULL);
+    /* CSS */
+    GtkCssProvider *cssProvider = gtk_css_provider_new();
+    if( gtk_css_provider_load_from_path(cssProvider, "/usr/share/dictpopup/textview.css", NULL) ) 
+    {
+     gtk_style_context_add_provider(gtk_widget_get_style_context(textview),
+                                        GTK_STYLE_PROVIDER(cssProvider),
+                                        GTK_STYLE_PROVIDER_PRIORITY_USER);
+    }
+
+    gtk_container_add(GTK_CONTAINER(window), textview);
     
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_MOUSE);
     gtk_widget_show_all(window);
